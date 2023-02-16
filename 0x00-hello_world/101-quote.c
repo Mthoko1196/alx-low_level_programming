91b2e8ef4f881ad95b97d072cedc48d15d149e9e
Mthoko1196 <92365100+Mthoko1196@users.noreply.github.com>
2023-02-16 23:23:30 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /** 
- *main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *followed by a new line, to the standard error.
- *Return: Always 1.
+ *main - prints "and that piece of art is useful
+ *
+ *Return: Always 0 (Success)
  */
 
 int main(void)
