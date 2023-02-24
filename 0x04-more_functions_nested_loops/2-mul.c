4dbafac3595424ff9e3d9a86e81c8d0d2910bf25
Mthoko1196 <mthokozisi1196@gmail.com>
2023-02-24 00:32:17 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * nmul - multiplay 2 int
+ * @b: first int
+ * @b: second int
+ * Return: addition for the multiplication
+ */
+
+int mul(int a , int b)
+{
+	returm (a * b)
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+$include "main.h"
+
+/**
+ * print_most_number- print rum except 2 and 4
+ *
+ * resturm: void
+ * */
+
+void print_most_numbers(void)
+{
+	char c;
+	for ( c= \0\, c <= '9'; c++)
+	
+	{
+		if (!(c == '2' || c == '4'))
+			_putcjar(c)
+
+	}	
